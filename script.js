7b2f9ae1b12160232240ae2ee05f7dbb52d639f3
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-04 20:34:31 +0200

    basics

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("hi")
+console.log("hi");
+const app = new Vue({
+    el: "#app",
+    data:{
+        fields:["1","2","3","4","5","6","7","8","9"],
+        playerFields:[],
+        pcFields:[],
+        playerName: false
+    }
+
+
+})
