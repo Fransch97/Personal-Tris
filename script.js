c30de85ba5547dbb5cc70b5355db05918d72b563
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-05-04 20:56:34 +0200

    first function works

--- script.js
+++ script.js
@@ -5,8 +5,35 @@ const app = new Vue({
         fields:["1","2","3","4","5","6","7","8","9"],
         playerFields:[],
         pcFields:[],
-        playerName: false
-    }
+        playerNameStatus: false,
+        playerName : "",
+        winCombis:[[1,2,3],[4,5,6],[7,8,9],[3,6,9],[2,5,8],[1,4,7],[1,5,9],[3,5,7]]
+    },
 
+    methods: {
+        randomNumber(){
+            return Math.ceil(Math.random()*this.fields.length)
+        }
+    },
 
+    mounted() {
+        console.log(this.randomNumber())
+        const testWin = [2,1,3]
+       
+        
+    },
 })
+
+
+// WIN CONTROLL FUNCTION NEED ONE FORE MORE
+// let controll = 0
+        // for(number of testWin){
+        //     console.log("number", number)
+        //     if(this.winCombis[0].includes(number)){
+        //         console.log("si")
+        //         controll++
+        //         if(controll === 3){
+        //             console.log("win!")
+        //         }
+        //     }
+        // }
